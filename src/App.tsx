c8811c1de7eeb2e2912a7bbc2be3179d60023bf2
luankaio <luankaiosilveira45@hotmail.com>
2024-04-01 17:59:02 -0300

    b

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ function App() {
   const [mu, setMu] = useState("");
   const [me, setMe] = useState(true);
   const [mo, setMo] = useState("");
+  
 
+  //crio a task como um objeto, adicionando id e o valor inserido
   const criar = () => {
     const task = {
       id: mi,
@@ -20,45 +22,54 @@ function App() {
     setMa([...ma, task]);
   }
 
+  //exclui a task de acordo com o id
   const remove = (namee: any) => {
     setMa(ma.filter((name) => name.id !== namee))
 
   };
 
+  //recebo os valores digitados no input de forma constante
   const handle = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     setMu(event.target.value)
   }
 
-  const handleclickbutton = () =>{
-    setMu("")
-    criar()
+  //verifico se o botão clicado foi enter, se sim crio task
+  const clicarComEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    {event.key==='Enter'&&criarTask()}
   }
 
-  const handlepoppup = (name : any) =>{
+  //crio a task e verifico se é vazio ou contém apenas espaços
+  const criarTask = () =>{ 
+    {mu.trim()!=="" && criar(); setMu("")}
+    
+  }
+
+  //Crio um modal
+  const handlepopup = (name : any) =>{
     setMe(!me)
     setMo(name.taskk)
   }
-  const aaa = () =>{
 
-  }
   return (
     <div className="App">
-      <header>
-      <input value={mu} onChange={handle}></input>
-      <button onClick={handleclickbutton}>criar</button>
-      <h1>{"Tasklist"}</h1>
-      {me ? <div></div>:<div className='Oii' onClick={() => setMe(!me)}><h1 className='textinside2'>{mo}</h1></div>}
-      </header>
       
+      <header>
+      <input onKeyDown={clicarComEnter} value={mu} onChange={handle}></input>
+      <button onClick={criarTask}>criar</button>
+      <h1>{"Tasklist"}</h1>
+      
+      </header>
+      {me ? <div></div>:<div className='Oii' onClick={() => setMe(!me)}><h1 className='textinside2'>{mo}</h1></div>}
         <div className='pai'>{  
           
           ma.map((name,key) => {
             return <div className='card'>
               
               <div className='excluir' onClick={()=>remove(name.id)}></div>
-              <div className='Oi' onClick={() => handlepoppup(name)} ><h1 className='textinside'>{name.taskk}</h1></div> 
+              <div className='Oi' onClick={() => handlepopup(name)} ><h1 className='textinside'>{name.taskk}</h1></div> 
             </div>
-          })}
+          })
+          }
         </div>
     </div>
   );
